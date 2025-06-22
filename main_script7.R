49cddb8b7ffa7cd7e88dab4f3de7571e26e3c8cd
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2025-06-22 11:34:13 -0500

    Update main_script7.R

--- main_script7.R
+++ main_script7.R
@@ -8,7 +8,7 @@
 cat("Working directory:", getwd(), "\n")
 
 # Set up and confirm output folder
-output_dir <- file.path(getwd(), "outputs/script2")
+output_dir <- file.path(getwd(), "outputs/script7")
 dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
 cat("Created directory:", output_dir, "\n")
 
